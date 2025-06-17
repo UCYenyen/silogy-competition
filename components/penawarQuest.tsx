e3caf0fd8e09cd0c68b6a511457c67d7c5712372
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-06-17 15:00:19 +0700

    attempt lagi

--- components/penawarQuest.tsx
+++ components/penawarQuest.tsx
@@ -14,20 +14,22 @@ export default function PenawarCard({
   const handleTerima = async () => {
     try {
       // Update status_penerimaan di table users_permintaan
-      await supabase
+      const { error: error1 } = await supabase
         .from('users_permintaan')
         .update({ status_penerimaan: 'Diterima' })
         .eq('calon_penerima_id', penerima_id);
 
       // Update status_permintaan dan penerima_id di table permintaan
-      await supabase
+      const { error: error2 } = await supabase
         .from('permintaan')
         .update({ status_permintaan: 'sedang dikerjakan', penerima_id: penerima_id })
         // .eq('id', permintaanId); // tambahkan kondisi sesuai kebutuhan
 
+      if (error1 || error2) throw error1 || error2;
+
       alert('Penawaran berhasil diterima');
     } catch (error) {
-      alert('Terjadi kesalahan saat menerima, tolong cek koneksi internet anda');
+      alert('Terjadi kesalahan saat menerima, tolong cek koneksi internet anda. Error code : ' + error);
     }
   };
 
@@ -44,7 +46,7 @@ export default function PenawarCard({
       alert('Penawaran berhasil ditolak');
       window.location.reload(); // Reload the page to reflect changes
     } catch (error) {
-      alert('Terjadi kesalahan saat menolak, tolong cek koneksi internet anda');
+      alert('Terjadi kesalahan saat menolak, tolong cek koneksi internet anda. Error code : ' + error);
     }
   };
 
