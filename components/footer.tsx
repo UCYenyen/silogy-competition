602b46157af7e4c9b4e730632b3462596856104e
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-06-17 14:00:26 +0700

    footer fix bug i just removed the animation

--- components/footer.tsx
+++ components/footer.tsx
@@ -29,24 +29,24 @@ export default function Footer(){
     return(
         <div className="bg-[#93CBDC] px-12 md:px-24 w-full min-h-[25vh] flex justify-between items-center gap-4">
             <div className="flex flex-col gap-4 text-[#322C2C]">
-                <h1 className="footer-reveal font-heading text-4xl sm:text-6xl md:text-8xl font-semibold">TolongYuk!</h1>
-                <h1 className="footer-reveal text-xs sm:text-lg md:text-xl w-[80%] font-semibold">© 2025 TolongYuk by Rebet. All Rights Reserved.</h1>
+                <h1 className="font-heading text-4xl sm:text-6xl md:text-8xl font-semibold">TolongYuk!</h1>
+                <h1 className="text-xs sm:text-lg md:text-xl w-[80%] font-semibold">© 2025 TolongYuk by Rebet. All Rights Reserved.</h1>
             </div>
             <div className="flex gap-8">
                 <div className="flex flex-col gap-4">
                     <h1 className="text-[#322C2C] footer-reveal font-bold text-xs sm:text-lg md:text-xl">Links</h1>
                     <div className="flex flex-col gap-4 text-xs sm:text-lg md:text-xl">
-                        <Link className="footer-reveal" href="/">Home</Link>
-                        <Link className="footer-reveal" href="/">Permintaan</Link>
-                        <Link className="footer-reveal" href="/">Contact</Link>
+                        <Link className="" href="/">Home</Link>
+                        <Link className="" href="/">Permintaan</Link>
+                        <Link className="" href="/">Contact</Link>
                     </div>
                 </div>
                 <div className="flex flex-col gap-4">
                     <h1 className="text-[#322C2C] footer-reveal font-bold text-xs sm:text-lg md:text-xl">About Us</h1>
                     <div className="flex flex-col gap-4 text-xs sm:text-lg md:text-xl">
-                        <Link className="footer-reveal" href="/">Instagram</Link>
-                        <Link className="footer-reveal" href="/">Youtube</Link>
-                        <Link className="footer-reveal" href="/">Twitter</Link>
+                        <Link className="" href="/">Instagram</Link>
+                        <Link className="" href="/">Youtube</Link>
+                        <Link className="" href="/">Twitter</Link>
                     </div>
                 </div>
             </div>
